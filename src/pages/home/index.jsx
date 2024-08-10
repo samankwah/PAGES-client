603efbf7419d2ed5d49282c6c2df524ca6508a39
1amankwah <stephen.amankwah@meteo.gov.gh>
2024-08-10 15:51:03 +0000

    fix height issues

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import ChatDialog from "../../components/interfaces/ChatDialog";
 import Videos from "../../components/interfaces/Videos";
@@ -7,8 +7,24 @@ import Call from "../../components/interfaces/Call";
 const Home = () => {
   const [interfaceToShow, setInterfaceToShow] = useState("chat");
 
+  useEffect(() => {
+    const handleResize = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    // Initial call to set the value
+    handleResize();
+
+    // Update the value on resize
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="relative bg-gray-100 shadow-xl border lg:max-w-lg mx-auto md:rounded-xl h-screen flex flex-col">
+    <div className="relative bg-gray-100 shadow-xl border lg:max-w-lg mx-auto md:rounded-xl full-height flex flex-col">
+      {/* <div className="relative bg-gray-100 shadow-xl border lg:max-w-lg mx-auto md:rounded-xl h-screen flex flex-col"> */}
       <Navbar
         interfaceToShow={interfaceToShow}
         setInterfaceToShow={setInterfaceToShow}
