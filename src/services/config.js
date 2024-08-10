bdebe6baa8c3ff2128277fbe9f15bb23d1bc8543
1amankwah <stephen.amankwah@meteo.gov.gh>
2024-08-10 08:15:41 +0000

    refactor final url

--- src/services/config.js
+++ src/services/config.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const proxyUrl = "https://pages-client.netlify.app/";
+const targetUrl = "https://pages-hkb.onrender.com";
+const finalUrl = proxyUrl + targetUrl;
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
 export const apiClient = axios.create({
-  baseURL: "https://pages-hdkb.onrender.com",
+  baseURL: finalUrl,
   withCredentials: true,
 });
