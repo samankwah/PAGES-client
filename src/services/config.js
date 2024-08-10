2aa796b17bbf84a19533279795b5f6160511650c
1amankwah <stephen.amankwah@meteo.gov.gh>
2024-08-10 08:27:54 +0000

    refactor redirects file

--- src/services/config.js
+++ src/services/config.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const proxyUrl = "https://pages-client.netlify.app/";
-const targetUrl = "https://pages-hkb.onrender.com";
-const finalUrl = proxyUrl + targetUrl;
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
 export const apiClient = axios.create({
-  baseURL: finalUrl,
+  baseURL: "https://pages-hdkb.onrender.com",
   withCredentials: true,
 });
