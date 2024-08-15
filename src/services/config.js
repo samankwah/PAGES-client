fbc467923eed802cfc085693631cf604af62a01d
1amankwah <stephen.amankwah@meteo.gov.gh>
2024-08-15 09:38:59 +0000

    refactor

--- src/services/config.js
+++ src/services/config.js
@@ -5,6 +5,6 @@ const baseUrl = import.meta.env.VITE_BASE_URL;
 export const apiClient = axios.create({
   // baseURL: baseUrl, ----------working in production----------
   // baseURL: "/api", ----------working in development----------
-  baseURL: "/api",
+  baseURL: baseUrl,
   // withCredentials: true,
 });
